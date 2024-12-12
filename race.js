a87b274fd7bf05837d9a16e010ea7f8d84ebe49d
jicho <jIcho6412@gmail.com>
2024-12-12 15:10:33 +0900

    문제 - 달리기 경주
    객체를 사용한 문제 풀이 추가

--- race.js
+++ race.js
@@ -1,5 +1,5 @@
 function solution(players, callings) {
-  var answer = players;
+  //var answer = players;
   // 해결 1)
   // for (let i = 0; i < callings.length; i++) {
   //   let order = answer.indexOf(callings[i]);
@@ -9,14 +9,34 @@ function solution(players, callings) {
   // }
 
   // 해결 2)
-  for (let i = 0; i < callings.length; i++) {
-    let order = answer.indexOf(callings[i]); // order = 3
-    let item = answer[order]; // kai
-    answer.splice(order, 1, answer[order - 1]);
-    answer.splice(order - 1, 1, item);
+  // for (let i = 0; i < callings.length; i++) {
+  //   let order = answer.indexOf(callings[i]); // order = 3
+  //   let item = answer[order]; // kai
+  //   answer.splice(order, 1, answer[order - 1]);
+  //   answer.splice(order - 1, 1, item);
+  // }
+
+  //return answer;
+
+  // 해결 3)
+  let playerIndex = {};
+  for (let i = 0; i < players.length; i++) {
+    playerIndex[players[i]] = i;
   }
 
-  return answer;
+  for (let i = 0; i < callings.length; i++) {
+    let player = callings[i];
+    let idx = playerIndex[player];
+
+    let temp = players[idx - 1];
+    players[idx - 1] = player;
+    players[idx] = temp;
+
+    playerIndex[player] = idx - 1;
+    playerIndex[temp] = idx;
+  }
+
+  return players;
 }
 
 console.log(
