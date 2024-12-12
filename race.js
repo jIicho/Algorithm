4c5c85f70202a748860f6147c067356e94399fbb
jicho <jIcho6412@gmail.com>
2024-12-12 14:59:32 +0900

    문제 - 달리기 경주
    splice를 사용한 풀이 방법 추가

--- race.js
+++ race.js
@@ -1,11 +1,19 @@
 function solution(players, callings) {
   var answer = players;
   // 해결 1)
+  // for (let i = 0; i < callings.length; i++) {
+  //   let order = answer.indexOf(callings[i]);
+  //   let item = answer[order];
+  //   answer[order] = answer[order - 1];
+  //   answer[order - 1] = item;
+  // }
+
+  // 해결 2)
   for (let i = 0; i < callings.length; i++) {
-    let order = answer.indexOf(callings[i]);
-    let item = answer[order];
-    answer[order] = answer[order - 1];
-    answer[order - 1] = item;
+    let order = answer.indexOf(callings[i]); // order = 3
+    let item = answer[order]; // kai
+    answer.splice(order, 1, answer[order - 1]);
+    answer.splice(order - 1, 1, item);
   }
 
   return answer;
