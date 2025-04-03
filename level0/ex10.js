c635fb2dc6c0117f2e2772a16a44fdf254bbd95b
jicho <jIcho6412@gmail.com>
2025-04-03 11:33:30 +0900

    문자열 여러 번 뒤집기 - 다른 풀이

--- level0/ex10.js
+++ level0/ex10.js
@@ -1,4 +1,4 @@
-function solution(my_string, queries) {
+function solution1(my_string, queries) {
   var answer = my_string.split("");
   let result = "";
   for (let x of queries) {
@@ -11,8 +11,30 @@ function solution(my_string, queries) {
   return answer.join("");
 }
 
+// 다른 풀이
+
+function solution2(my_string, queries) {
+  let answer = "";
+  let my_stringArr = my_string.split("");
+  queries.forEach((numArr) => {
+    let result = my_stringArr.slice(numArr[0], numArr[1] + 1).reverse();
+    my_stringArr.splice(numArr[0], numArr[1] - numArr[0] + 1);
+    my_stringArr.splice(numArr[0], 0, ...result);
+  });
+  answer = my_stringArr.join("");
+  return answer;
+}
+
 console.log(
-  solution("rermgorpsam", [
+  solution1("rermgorpsam", [
+    [2, 3],
+    [0, 7],
+    [5, 9],
+    [6, 10],
+  ])
+);
+console.log(
+  solution2("rermgorpsam", [
     [2, 3],
     [0, 7],
     [5, 9],
